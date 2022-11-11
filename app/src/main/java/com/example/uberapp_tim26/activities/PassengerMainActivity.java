5653bf976590d9d48153ff8771545e00730c1118
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2022-11-11 23:39:06 +0100

    override activity methods

--- app/src/main/java/com/example/uberapp_tim26/activities/PassengerMainActivity.java
+++ app/src/main/java/com/example/uberapp_tim26/activities/PassengerMainActivity.java
@@ -1,8 +1,8 @@
 package com.example.uberapp_tim26.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.uberapp_tim26.R;
 
@@ -12,5 +12,35 @@ public class PassengerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
