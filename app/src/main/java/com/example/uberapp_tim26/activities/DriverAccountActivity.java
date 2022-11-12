fa38d7a8a78f792e3c541dc15d4c7ec3f01078b2
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2022-11-12 18:11:15 +0100

    Create Empty Activity for driver account

---
+++ app/src/main/java/com/example/uberapp_tim26/activities/DriverAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.uberapp_tim26.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uberapp_tim26.R;
+
+public class DriverAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_account);
+    }
+}
