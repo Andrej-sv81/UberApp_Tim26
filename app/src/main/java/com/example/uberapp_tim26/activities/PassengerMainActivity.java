7362f1cce304c7b88b768961a61dc4aa80db30b9
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2022-11-12 15:51:25 +0100

    Added toolbar icons and action; added empty history activity

--- app/src/main/java/com/example/uberapp_tim26/activities/PassengerMainActivity.java
+++ app/src/main/java/com/example/uberapp_tim26/activities/PassengerMainActivity.java
@@ -1,11 +1,16 @@
 package com.example.uberapp_tim26.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.uberapp_tim26.R;
 
@@ -15,6 +20,9 @@ public class PassengerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPassengerMain);
+        setSupportActionBar(toolbar);
 
         Button register = findViewById(R.id.buttonAccount);
         register.setOnClickListener(new AccountListener());
@@ -67,5 +75,37 @@ public class PassengerMainActivity extends AppCompatActivity {
             Intent i = new Intent(PassengerMainActivity.this, PassengerInboxActivity.class);
             startActivity(i);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        getMenuInflater().inflate(R.menu.menu_passenger_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.passengerHome:
+                Intent i = new Intent(PassengerMainActivity.this, PassengerMainActivity.class);
+                startActivity(i);
+                break;
+            case R.id.passengerAccount:
+                Intent i2 = new Intent(PassengerMainActivity.this, PassengerAccountActivity.class);
+                startActivity(i2);
+                break;
+            case R.id.passengerInbox:
+                Intent i3 = new Intent(PassengerMainActivity.this, PassengerInboxActivity.class);
+                startActivity(i3);
+                break;
+            case R.id.passengerHistory:
+                Intent i4 = new Intent(PassengerMainActivity.this, PassengerHistoryActivity.class);
+                startActivity(i4);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
