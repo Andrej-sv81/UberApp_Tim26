234641013bb217937bd0e3c179ca2300c3516b38
Danilo-Babic <daniloraska@gmail.com>
2022-11-14 14:32:20 +0100

    Splash screen added
    Changed background on splash screen, TODO: change activity after splash screen

---
+++ app/src/main/java/com/example/uberapp_tim26/activities/DriverAcceptanceRideActivity.java
@@ -0,0 +1,16 @@
+package com.example.uberapp_tim26.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uberapp_tim26.R;
+
+public class DriverAcceptanceRideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_acceptance_ride);
+    }
+}
